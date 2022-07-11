8ed32d0ec5d62ad19a42d59c610b18f8b5453676
emirhanyener <yeneremirhann@gmail.com>
2022-07-11 22:41:35 +0300

    update game for ai

--- js/Character.js
+++ js/Character.js
@@ -5,6 +5,7 @@ class Character{
         this.velocity_x = 0;
         this.velocity_y = 0;
         this.is_alive = true;
+        this.speed = 8;
         this.next = null;
     }
 
@@ -17,7 +18,7 @@ class Character{
     }
 
     calculate(){
-        this.position_x += this.velocity_x;
-        this.position_y += this.velocity_y;
+        this.position_x += this.velocity_x * this.speed;
+        this.position_y += this.velocity_y * this.speed;
     }
 }
--- js/index.js
+++ js/index.js
@@ -3,6 +3,7 @@ canvas.setAttribute('width', window.innerWidth * 0.8);
 canvas.setAttribute('height', window.innerHeight * 0.8);
 
 var ctx = canvas.getContext("2d");
+var info = document.getElementById("info-text");
 
 class Position {
 	constructor(x, y){
@@ -11,29 +12,21 @@ class Position {
 	}
 }
 
+let is_end = false;
+
 let secure_position = new Position(100, 100);
-
-let x = 0;
-let y = 100;
-let speed = 2;
 let score = 0;
-let is_alive = true;
-
-document.addEventListener('keydown', player_control_keydown);
-document.addEventListener('keyup', player_control_keyup);
-
+let generation = 100;
 setInterval(update, 10);
-setInterval(update_secure, 5000);
 
+let networks = [];
+for(let i = 0; i < generation; i++){
+	networks.push(new Network());
+}
 
-let list = new Character(300, 300);
-let iterator = new CharacterIterator(list);
-
-for(let i = 0; i < 49; i++){
-	let temp = new Character(300, 300);
-	temp.velocity_x = (Math.random() - 0.5) * 10;
-	temp.velocity_y = (Math.random() - 0.5) * 10;
-	list.add(temp);
+let characters = [];
+for(let i = 0; i < generation; i++){
+	characters.push(new Character(400, 300));
 }
 
 let right = false;
@@ -41,83 +34,85 @@ let left = false;
 let up = false;
 let down = false;
 
-function list_characters(){
-	iterator.reset();
-
-	while(iterator.next())
-	{
-		console.log(iterator.get());
+start();
+alive = generation;
+function start(){
+	for(let i = 0; i < generation; i++){
+		networks[i].add_layer([4, 2]);
 	}
 }
 
+const time_delay = 800;
+let saved_time = (new Date()).getTime();
+
 function update(){
-	if(left){
-		x -= speed;
+	const d = new Date();
+	if(d.getTime() - saved_time > time_delay){
+		update_secure();
+		saved_time = d.getTime();
 	}
-	if(right){
-		x += speed;
+
+	if(alive == 0){
+		reset_all();
+		return;
 	}
-	if(up){
-		y -= speed;
-	}
-	if(down){
-		y += speed;
-	}
+
 	ctx.fillStyle = "#000000";
 	ctx.fillRect(0, 0, window.innerWidth * 0.8, window.innerHeight * 0.8);
 	ctx.fillStyle = "#009900";
 	ctx.fillRect(secure_position.x, secure_position.y, 200, 200);
 	
-	iterator.reset();
-	while(iterator.next()){
-		ctx.fillStyle = "#990000";
-		ctx.fillRect(iterator.get().position_x, iterator.get().position_y, 25, 25);
-		iterator.get().calculate();
+	for(let i = 0; i < generation; i++){
+		if(characters[i].is_alive){
+			networks[i].calculate([((characters[i].position_x) / (window.innerWidth * 0.8)), ((secure_position.x + 100) / (window.innerWidth * 0.8)), ((characters[i].position_y) / (window.innerHeight * 0.8)), ((secure_position.y + 100) / (window.innerHeight * 0.8))]);
+			characters[i].velocity_x = networks[i].layers[networks[i].layers.length - 1].neurons[0].value;
+			characters[i].velocity_y = networks[i].layers[networks[i].layers.length - 1].neurons[1].value;
+			characters[i].calculate();
+
+			ctx.fillStyle = "#990000";
+			ctx.fillRect(characters[i].position_x, characters[i].position_y, 25, 25);
+		}
 	}
 
 	score++;
 	document.getElementById("score-text").innerHTML = "score = " + score;
-	if(!is_alive)
-		document.getElementById("status-text").innerHTML = "status = dead";
+	document.getElementById("info-text").innerHTML = "alive = " + alive;
+}
+
+function reset_all(){
+	update_secure();
+	saved_time = (new Date()).getTime();
+	score = 0;
+	alive = generation;
+	
+	for(let i = 0; i < generation; i++){
+		characters[i].position_x = 500;
+		characters[i].position_y = 250;
+		characters[i].is_alive = true;
+		networks[i].next([((characters[i].position_x) / (window.innerWidth * 0.8)), ((secure_position.x + 100) / (window.innerWidth * 0.8)), ((characters[i].position_y) / (window.innerHeight * 0.8)), ((secure_position.y + 100) / (window.innerHeight * 0.8))]);
+	}
 }
 
 function update_secure(){
-	if(Math.abs(secure_position.x + 100 - x) > 100 || Math.abs(secure_position.y + 100 - y) > 100)
-		is_alive = false;
+	
+	for(let i = 0; i < generation; i++){
+		if(Math.abs(secure_position.x + 100 - characters[i].position_x) > 100 || Math.abs(secure_position.y + 100 - characters[i].position_y) > 100){
+			if(characters[i].is_alive){
+				characters[i].is_alive = false;
+				alive--;
+			}
+		}
+		else{
+			if(characters[i].is_alive){
+				networks[i].next([((characters[i].position_x) / (window.innerWidth * 0.8)), ((secure_position.x + 100) / (window.innerWidth * 0.8)), ((characters[i].position_y) / (window.innerHeight * 0.8)), ((secure_position.y + 100) / (window.innerHeight * 0.8))]);
+			}
+		}
+	}
 
-	secure_position.x = Math.random() * window.innerWidth * 0.8;
-	secure_position.y = Math.random() * window.innerHeight * 0.8;
+	secure_position.x = Math.round(Math.random() * window.innerWidth * 0.8);
+	secure_position.y = Math.round(Math.random() * window.innerHeight * 0.8);
 	if(secure_position.x > window.innerWidth * 0.8 - 200)
 		secure_position.x -= 200;
 	if(secure_position.y > window.innerHeight * 0.8 - 200)
 		secure_position.y -= 200;
-}
-
-function player_control_keydown(e){
-	if(e.code == 'KeyA' || e.code == 'ArrowLeft'){
-		left = true;
-	}
-	if(e.code == 'KeyD' || e.code == 'ArrowRight'){
-		right = true;
-	}
-	if(e.code == 'KeyW' || e.code == 'ArrowUp'){
-		up = true;
-	}
-	if(e.code == 'KeyS' || e.code == 'ArrowDown'){
-		down = true;
-	}
-}
-function player_control_keyup(e){
-	if(e.code == 'KeyA' || e.code == 'ArrowLeft'){
-		left = false;
-	}
-	if(e.code == 'KeyD' || e.code == 'ArrowRight'){
-		right = false;
-	}
-	if(e.code == 'KeyW' || e.code == 'ArrowUp'){
-		up = false;
-	}
-	if(e.code == 'KeyS' || e.code == 'ArrowDown'){
-		down = false;
-	}
 }
